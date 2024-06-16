51ea3062d94a463522e8006099d2cb83b480182c
Ramads <sudiawanrama@gmai.com>
2024-06-16 18:28:25 +0700

    main js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-// Ini Javascript
+//script
+
+document.getElementById('convert').onclick = tempConvert;
+document.getElementById('clear').onclick = clearForm
+
+function tempConvert() {
+    var fahreinheit = document.getElementById(fahreinheit).value
+    var celcius = document.getElementById(celcius).value
+
+    if (fahreinheit != '') {
+        celcius = (parseFloat(fahreinheit) -32) /1.8;
+    } else{
+        fahreinheit = (parseFloat(celcius) * 1.8) + 32;
+    }
+
+    document.getElementById('fahrenheit').value = parseFloat(fahreinheit).toFixed(1);
+    document.getElementById('celcius').value = parseFloat(celcius).toFixed(1);
+}
+
+function clearForm() {
+    document.getElementById('fahrenheit').value = '';
+    document.getElementById('celcius').value = '';
+}
